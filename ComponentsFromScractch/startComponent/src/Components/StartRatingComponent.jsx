c94700a2401114f5cab02be2b912291465a718ae
sudhanshu singh <ss826063@gmail.com>
2024-10-02 23:53:33 +0530

    startComponent change

--- ComponentsFromScractch/startComponent/src/Components/StartRatingComponent.jsx
+++ ComponentsFromScractch/startComponent/src/Components/StartRatingComponent.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function StartRating ({startCount=5}){
-    const [startValue,setStarValue] = useState();
-    const [hoverIndex,setHoverIndex] = useState();
-    console.log("star value is ",startValue);
-    return <>
-    <div className="container">
-        {new Array(startCount).fill(0).map((value,index)=> {
-           return  <span key={index} className={hoverIndex ===0 && index < startValue || index <  hoverIndex ? "gold":"star"} onClick={()=>{setStarValue(index+1)}} onMouseEnter={()=>{setHoverIndex(index+1)}} onMouseLeave={()=>setHoverIndex(0)} >&#9733;</span>
+export default function StartRating({ startCount = 5 }) {
+  const [startValue, setStarValue] = useState();
+  const [hoverIndex, setHoverIndex] = useState();
+  console.log("star value is ", startValue);
+  return (
+    <>
+      <div className="container">
+        {new Array(startCount).fill(0).map((value, index) => {
+          return (
+            <span
+              key={index}
+              className={
+                (hoverIndex === 0 && index < startValue) || index < hoverIndex
+                  ? "gold"
+                  : "star"
+              }
+              onClick={() => {
+                setStarValue(index + 1);
+              }}
+              onMouseEnter={() => {
+                setHoverIndex(index + 1);
+              }}
+              onMouseLeave={() => setHoverIndex(0)}
+            >
+              &#9733;
+            </span>
+          );
         })}
-    </div>
+      </div>
     </>
+  );
 }
